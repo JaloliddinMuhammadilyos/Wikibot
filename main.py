266d8de424268241ae0f2c85945e4bfb13df6294
Jaloliddin Muhammad <jaloliddinbz@gmail.com>
2022-06-03 16:46:18 +0500

    git commit -m "initial"

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from telegram.bot import Bot
 from telegram.user import User
 from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext
 from telegram.update import Update
+from settings import settings
+
 
 '''
 
-bot = Bot(token="5573637035:AAHdNMvfB0MvfnJMuyWjTmEFLLoG8cX_H7U")
+bot = Bot(token="")
 # print(bot.get_me())
 user: User = bot.get_me()
 # print(user.link)
 '''
 
-updater = Updater(token="5573637035:AAHdNMvfB0MvfnJMuyWjTmEFLLoG8cX_H7U")
+updater = Updater(token=settings.TELEGRAM_TOKEN)
 """python telegram bot update qilishi uchun updateni qo'ydik"""
 
 
@@ -19,6 +21,10 @@ updater = Updater(token="5573637035:AAHdNMvfB0MvfnJMuyWjTmEFLLoG8cX_H7U")
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Salom")
+    context.bot.send_message(chat_id=update.message.chat_id, text = "Salom yana bir bor!")
+        # message classga o'rab berdi
+
+
 
 dispatcher = updater.dispatcher                            # Updater dispatcherni ichida keladi
 dispatcher.add_handler(CommandHandler("start", start))     # Va dispatcherga Handler qo'shamiz. masalan commandalarni handler qiladi
